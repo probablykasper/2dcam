e5d1f73d6ba4c2426c44d4597f23b2c2e71cd2e2
Kasper <kasperkh.kh@gmail.com>
2020-03-09 23:33:34 +0100

    Stopped using resource strings

--- 2dcam.jsx
+++ 2dcam.jsx
@@ -5,71 +5,136 @@ icons = {
   camera: "\u0089PNG\r\n\x1A\n\x00\x00\x00\rIHDR\x00\x00\x00\x1A\x00\x00\x00\x1A\b\x06\x00\x00\x00\u00A9JL\u00CE\x00\x00\x01\u0082iCCPsRGB IEC61966-2.1\x00\x00(\u0091u\u0091\u00CBKBA\x14\u0087?5)\u00CA0(\u00A2E\x0B\tkea\x05Q\x04AJX !f\u00D0k\u00A37\x1F\u0081\u00DA\u00E5^#\u00A4m\u00D0V(\u0088\u00DA\u00F4Z\u00D4_P\u00DB\u00A0u\x10\x14E\x10\u00ADZ\u00B4.jSr;7\x05%\u00F2\fg\u00CE7\u00BF\u0099s\u00989\x03\u00D6HZ\u00C9\u00E8u^\u00C8dsZ8\u00E0s\u00CD\u00CD/\u00B8\u00EA_\u00B0\u00D1@;vF\u00A3\u008A\u00AE\u008E\u0087BAj\u00DA\u00E7=\x163\u00DE\u00F6\u009A\u00B5j\u009F\u00FB\u00D7\u009A\u0096\u00E3\u00BA\x02\u0096\x06\u00E11E\u00D5r\u00C2\u0093\u00C2\u00C1\u00F5\u009Cj\u00F2\u008Ep\u009B\u0092\u008A.\x0B\u009F\t{4\u00B9\u00A0\u00F0\u009D\u00A9\u00C7J\u00FCjr\u00B2\u00C4\u00DF&k\u0091\u00B0\x1F\u00AC-\u00C2\u00AEd\x15\u00C7\u00AAXIi\x19ay9\u00EELzM)\u00DF\u00C7|\u0089#\u009E\u009D\u009D\u0091\u00D8%\u00DE\u0089N\u0098\x00>\\L1\u0081\u009F!\u00FA\x19\u0091y\u0088^\x06\u00E8\u0093\x155\u00F2\u00BD\u00BF\u00F9\u00D3\u00ACJ\u00AE\"\u00B3J\x1E\u008D\x15\u0092\u00A4\u00C8\u00E1\x11uM\u00AA\u00C7%&D\u008F\u00CBH\u00937\u00FB\u00FF\u00B7\u00AFzbp\u00A0T\u00DD\u00E1\x03\u00FB\u00B3a\u00BCwC\u00FD6\x14\x0B\u0086\u00F1ud\x18\u00C5c\u00B0=\u00C1e\u00B6\u0092\u00BFz\b\u00C3\x1F\u00A2\x17*\u009A\u00FB\x00\u009C\u009Bp~U\u00D1b\u00BBp\u00B1\x05\x1D\u008FjT\u008B\u00FEJ6qk\"\x01o\u00A7\u00D0<\x0F\u00AD7\u00D0\u00B8X\u00EAYy\u009F\u0093\x07\u0088l\u00C8W]\u00C3\u00DE>\u00F4\u00C8y\u00E7\u00D2\x0FHkg\u00D8X\u00BD\u00A4\u00BC\x00\x00\x00\tpHYs\x00\x00\x0B\x13\x00\x00\x0B\x13\x01\x00\u009A\u009C\x18\x00\x00\x00\u0081IDATH\u0089\u00ED\u0094;\x12\u00C3 \f\x05\u00F52\u00E3\x1B\u0085>\u0097\u00F2\u00F9 '\u00DB4\u00A10\u00B6\x07\f1i\u00B4\u00A5\x10Z\r\x1F\u00999\u00CE_\x00\x02\u0090\x18#\x01\u00A1&J\u00C0\n,\u009D\u008D.\u00DF\u00FD\u00B1\u0096H\u00AF\u00A4\u0090Q\u00C4^@\u00DA\u0088F$e\u009D,\u00C8gz\u008B\u00E8\u00E8\u00AE\u00CD\u00CC\u0094\x13$\u00E9\x17\u00A2\u00A3\u00B8$=F\u008B\u00B7\u00E2\u00A2\x16\u00DE\u00A7+S\u009F\u00F7\u00AC\x0F;m\x04\x05 ^\x1A\u00A1{\"\u00F0\u00ECi\u00D4q\u00EE\u00E1\x03?\u009D8B\u00C4\u00BC<\u00F7\x00\x00\x00\x00IEND\u00AEB`\u0082",
 }
 
-res = 'group{ \
-  orientation:"row",\
-  newCamButton: IconButton{ \
-    properties: {style:"toolbutton"}, \
-    preferredSize: [30, 30], \
-    helpTip: "New 2dCam\\nAlt+click: New 2dCam and output comp", \
-  }, \
-  newOutputButton: IconButton{ \
-    properties: {style:"toolbutton"}, \
-    preferredSize: [30, 30], \
-    helpTip: "New 2dCam output comp\\nAlt+click: Apply 2dCam output to the selected layer" \
-  }, \
-}';
-win.grp = win.add(res);
+Object.prototype.addc = function(name, options) {
+  options = options || {};
+  var bounds = options.bounds;
+  var properties = options.properties;
 
-win.grp.newCamButton.image = ScriptUI.newImage(icons.camera);
-// win.grp.newCamButton.image = ScriptUI.newImage('./icons/camera.png');
-win.grp.newOutputButton.image = ScriptUI.newImage(icons.screen);
-// win.grp.newOutputButton.image = ScriptUI.newImage('./icons/screen.png');
+  var noArg2 = false;
+  var arg2 = options.text;
+  if (name == 'dropdownlist' || name == 'listbox' || name == 'treeview') {
+    arg2 = options.items;
+  } else if (name == 'iconbutton' || name == 'image') {
+    arg2 = options.icon;
+  } else if (name == 'flashplayer') {
+    arg2 = options.movieToLoad;
+  } else if (name == 'group') {
+    noArg2 = true;
+  }
+
+  if (noArg2) {
+    newElement = this.add(name, bounds, properties);
+  } else if (name == 'progressbar' || name == 'scrollbar' || name == 'slider') {
+    newElement = this.add(name, bounds, options.value, options.minvalue, options.maxvalue, properties);
+  } else {
+    newElement = this.add(name, bounds, arg2, properties);
+  }
+  
+  // set options
+  for (key in options) {
+    if (!options.hasOwnProperty(key)) continue;
+    newElement[key] = options[key];
+  }
+  
+  return newElement;
+}
+
+var rootGroup = win.addc('group', {
+  orientation: 'row',
+});
+var newCamButton = rootGroup.addc('iconbutton', {
+  properties: {style: 'toolbutton'},
+  icon: icons.camera,
+  // icon: './icons/camera.png',
+  preferredSize: [30,30],
+  helpTip: 'New 2dCam\nAlt+click: New 2dCam and output comp',
+});
+var newOutputButton = rootGroup.addc('iconbutton', {
+  properties: {style: 'toolbutton'},
+  icon: icons.screen,
+  // icon: './icons/screen.png',
+  preferredSize: [30,30],
+  helpTip: 'New 2dCam output comp\nAlt+click: Apply 2dCam output to the selected layer',
+});
+
+var nameFieldWidthChars = 20;
 
 function newCamDialog(currentComp, alt, ok) {
   var dialog = new Window("dialog", "New 2dCam", undefined, { resizeable: false });
 
-  var width = currentComp.width;
-  var height = currentComp.height;
-  res = 'group{ \
-    orientation:"column", \
-    panelGroup: Panel{ \
-      orientation:"column", \
-      alignChildren:"left", \
-      nameGroup: Group{orientation:"row", \
-        nameText: StaticText{text:"Name:", justify:"right", preferredSize: [40, 20]}, \
-        name: EditText{text:"2dCam", characters:15}, \
-      }, \
-      widthGroup: Group{ \
-        orientation:"row", \
-        widthText: StaticText{text:"Width:", justify:"right", preferredSize: [40, 20]},\
-        width: EditText{text:"'+width+'", characters:6}, \
-      }, \
-      heightGroup: Group{ \
-        orientation:"row", \
-        heightText: StaticText{text:"Height:", justify:"right", preferredSize: [40, 20]}, \
-        height: EditText{text:"'+height+'", characters:6}, \
-      }, \
-      createOutputComp: Checkbox{text:"Create output comp", alignment:"center", value:'+alt+'}, \
-    }, \
-    buttonsGroup: Group{ \
-      orientation:"row", \
-      cancelButton: Button{text:"Cancel"}, \
-      okButton: Button{text:"OK"}, \
-    }, \
-  }';
+  var defaultWidth = currentComp.width;
+  var defaultHeight = currentComp.height;
 
-  dialog.grp = dialog.add(res);
-  dialog.grp.panelGroup.nameGroup.name.active = true;
+  var dialogRootGroup = dialog.addc('group', {
+    orientation: 'column',
+  });
+    var nameGroup = dialogRootGroup.addc('group', {
+      orientation: 'row',
+    });
+      var nameText = nameGroup.addc('statictext', {
+        text: 'Name:',
+        justify: 'right',
+        // preferredSize: [40, 20],
+      });
+      var name = nameGroup.addc('edittext', {
+        text: '2dCam',
+        active: true,
+        characters: nameFieldWidthChars,
+      });
+    var panelGroup = dialogRootGroup.addc('panel', {
+      // text: 'Options',
+      minimumSize: [200, 20],
+      orientation: 'column',
+      alignChildren: 'left',
+    });
+      var widthGroup = panelGroup.addc('group', {
+        orientation: 'row',
+      });
+        var widthText = widthGroup.addc('statictext', {
+          text: 'Width:',
+          justify: 'right',
+          preferredSize: [40, 20],
+        });
+        var width = widthGroup.addc('edittext', {
+          text: defaultWidth,
+          characters: 6,
+        });
+      var heightGroup = panelGroup.addc('group', {
+        orientation: 'row',
+      });
+        var heightText = heightGroup.addc('statictext', {
+          text: 'Height:',
+          justify: 'right',
+          preferredSize: [40, 20],
+        });
+        var height = heightGroup.addc('edittext', {
+          text: defaultHeight,
+          characters: 6,
+        });
+      var createOutputComp = panelGroup.addc('checkbox', {
+        text: 'Create output comp',
+        alignment: 'center',
+        value: alt,
+      });
+    var buttonsGroup = dialogRootGroup.addc('group', {
+      orientation: 'row',
+    });
+      var cancelButton = buttonsGroup.addc('button', {
+        text: 'Cancel',
+      });
+      var okButton = buttonsGroup.addc('button', {
+        text: 'OK',
+      });
 
-  dialog.grp.buttonsGroup.cancelButton.onClick = function() {
+  cancelButton.onClick = function() {
     dialog.close();
   }
-  dialog.grp.buttonsGroup.okButton.onClick = function() {
+  okButton.onClick = function() {
     var options = {
-      name: dialog.grp.panelGroup.nameGroup.name.text,
-      width: dialog.grp.panelGroup.widthGroup.width.text,
-      height: dialog.grp.panelGroup.heightGroup.height.text,
-      createOutputComp: dialog.grp.panelGroup.createOutputComp,
+      name: name.text,
+      width: width.text,
+      height: height.text,
+      createOutputComp: createOutputComp.value,
     };
     dialog.close();
     ok(options);
@@ -116,7 +181,7 @@ function newOutputComp(name, sourceComp) {
 }
 
 // New 2dCam
-win.grp.newCamButton.onClick = function() {
+newCamButton.onClick = function() {
   var currentComp = app.project.activeItem;
   if (!currentComp) return alert('Please select a comp');
 
@@ -157,34 +222,51 @@ win.grp.newCamButton.onClick = function() {
   });
 }
 
-function newOutputDialog(ok) {
+function newOutputDialog(nonCompLayersSelected, ok) {
   var dialog = new Window("dialog", "New 2dCam", undefined, { resizeable: false });
 
-  var width = currentComp.width;
-  var height = currentComp.height;
-  res = 'group{ \
-    orientation:"column", \
-    nameGroup: Group{orientation:"row", \
-      nameText: StaticText{text:"Name:", justify:"right", preferredSize: [40, 20]}, \
-      name: EditText{text:"2dCam", characters:15}, \
-    }, \
-    buttonsGroup: Group{ \
-      orientation:"row", \
-      cancelButton: Button{text:"Cancel"}, \
-      okButton: Button{text:"OK"}, \
-    }, \
-  }';
+  var dialogRootGroup = dialog.addc('group', {
+    orientation: 'column',
+  });
+    var nameGroup = dialogRootGroup.addc('group', {
+      orientation: 'row',
+    });
+      var nameText = nameGroup.addc('statictext', {
+        text: 'Name:',
+        justify: 'right',
+        preferredSize: [40, 20],
+      });
+      var name = nameGroup.addc('edittext', {
+        text: '2dCam',
+        active: true,
+        characters: nameFieldWidthChars,
+      });
+    var note = dialogRootGroup.addc('statictext', {
+      text: ' ',
+      alignment: 'center',
+      properties: { multiline: true },
+    });
+    note.graphics.foregroundColor = note.graphics.newPen(note.graphics.PenType.SOLID_COLOR, [1, 0.5, 0.1, 1], 1);
+    if (nonCompLayersSelected) {
+      note.text = 'Note: Non-comp layers have been selected, and will be skipped'
+    }
+    var buttonsGroup = dialogRootGroup.addc('group', {
+      orientation: 'row',
+    });
+      var cancelButton = buttonsGroup.addc('button', {
+        text: 'Cancel',
+      });
+      var okButton = buttonsGroup.addc('button', {
+        text: 'OK',
+      });
 
-  dialog.grp = dialog.add(res);
-  dialog.grp.nameGroup.name.active = true;
-
-  dialog.grp.buttonsGroup.cancelButton.onClick = function() {
+  cancelButton.onClick = function() {
     dialog.close();
   }
-  dialog.grp.buttonsGroup.okButton.onClick = function() {
-    var name = dialog.grp.nameGroup.name.text;
+  okButton.onClick = function() {
+    var finalName = name.text;
     dialog.close();
-    ok(dialog.grp.nameGroup.name.text);
+    ok(finalName);
   }
 
   dialog.layout.layout(true);
@@ -193,13 +275,13 @@ function newOutputDialog(ok) {
 }
 
 // New output
-win.grp.newOutputButton.onClick = function() {
+newOutputButton.onClick = function() {
   if (!ScriptUI.environment.keyboardState.altKey) {
     // new output comp
 
     var currentComp = app.project.activeItem;
     if (!currentComp) return alert('Please select a comp');
-    newOutputDialog(function(name) {
+    newOutputDialog(false, function(name) {
       app.beginUndoGroup('New 2dCam Output Comp');
       newOutputComp(name, currentComp);
       app.endUndoGroup();
@@ -231,12 +313,8 @@ win.grp.newOutputButton.onClick = function() {
     if (!compLayersSelected) {
       return alert('Please select one or more comp layers');
     }
-    // if (nonCompLayersSelected) {
-    //   proceed = confirm('Non-comp layers have been selected, and will be skipped. Proceed?');
-    //   if (!proceed) return;
-    // }
 
-    newOutputDialog(function(name) {
+    newOutputDialog(nonCompLayersSelected, function(name) {
       app.beginUndoGroup('Apply 2dCam Output');
       for (var i = 0; i < outputLayers.length; i++) {
         applyOutputToLayer(name, outputLayers[i]);
